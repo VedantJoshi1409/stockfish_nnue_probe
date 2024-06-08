648cbe5f7ab60f1cce21f58e1b39288532f9699b
Vedant Joshi <116992036+VedantJoshi1409@users.noreply.github.com>
2024-06-07 23:16:28 -0400

    Add files via upload

---
+++ NNUEBridge.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+public class NNUEBridge {
+    static {
+        File dll = new File("probe.dll");
+        System.load(dll.getAbsolutePath());
+    }
+
+    public static native void init(String bigNet, String smallNet);
+
+    public static native int evalFen(String fen);
+
+    public static native int evalArray(int[] pieceBoard, int side, int rule50);
+}
